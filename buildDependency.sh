ca5eb67e4df8cfd79c14f1cb0774d71418b289a2
xie xie <1515489649@qq.com>
2021-03-01 23:33:15 +0800

    rearranage

--- buildDependency.sh
+++ buildDependency.sh
@@ -1,35 +1,52 @@
 project_path=$(cd `dirname $0`; pwd)
 echo "Place enter the number you want do. 
-1:update ZLMediaKit 
-2:run in simulator
-3:run in iphone"
+1:build iphone
+2:build simulator
+3:merge to fat"
 
 
 derivedDataPath=$project_path/ZLMediaKit/build/derivedData
 desLibPath=$derivedDataPath/ArchiveIntermediates/ALL_BUILD/IntermediateBuildFilesPath/UninstalledProducts
-cd ZLMediaKit
+productPath=$project_path/IOSPlayer/SDKs
 
 read number
-
-if [ $number == 1 ]; then
 #git pull
 #git submodule update
-rm -rf build
-mkdir build
+
+if [ $number == 1 ]; then
+cd $project_path/ZLMediaKit
+# rm -rf build
+# mkdir build
 cd build
 cmake .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake  -DPLATFORM=OS
 project_name=$(ls ./ | grep '.xcodeproj')
-xcodebuild archive -project ${project_name} -scheme ALL_BUILD -configuration Debug -destination 'platform=iOS Simulator,name=iPhone 11' -derivedDataPath $derivedDataPath
-cmake .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake  -DPLATFORM=SIMULATOR
-xcodebuild archive -project ${project_name} -scheme ALL_BUILD -configuration Debug -destination 'platform=iOS Simulator,name=iPhone 11' -derivedDataPath $derivedDataPath
+xcodebuild archive -project ${project_name} -scheme ALL_BUILD -configuration Debug -destination 'platform=iOS,name=xo' -derivedDataPath $derivedDataPath
+rm -rf $productPath/iphoneos
+cp -r $desLibPath/iphoneos $productPath
 
-# sdks_path=${project_path}/IOSPlayer/SDKs
-# rm -rf $sdks_path
-# mkdir $sdks_path
+elif [ $number == 2 ]; then
 
+cd $project_path/ZLMediaKit
+rm -rf build
+mkdir build
+cd build
+cmake .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake  -DPLATFORM=SIMULATOR64
+project_name=$(ls ./ | grep '.xcodeproj')
+xcodebuild archive -project ${project_name} -scheme ALL_BUILD -configuration Debug -derivedDataPath $derivedDataPath
+rm -rf $productPath/iphonesimulator
+cp -r $desLibPath/iphonesimulator $productPath
 
-# lipo -create file.a file.b -output file.c
+elif [ $number == 3 ]; then
 
+rm -rf $productPath/iphoneuniversal
+mkdir $productPath/iphoneuniversal
+
+for file in $productPath/iphoneos/*
+do
+    filename=${file##*/}
+    lipo -create $file $productPath/iphonesimulator/$filename -output $productPath/iphoneuniversal/$filename
+done
+ln -s $productPath/iphoneuniversal $productPath/iphone
 else
 
 echo "end"
