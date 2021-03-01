7454fa0065fe9961e0a877634d46bd94137904f0
XIEY071 <yan.x.xie.-nd@disney.com>
2021-03-01 17:57:59 +0800

    update builddependency

--- buildDependency.sh
+++ buildDependency.sh
@@ -4,6 +4,9 @@ echo "Place enter the number you want do.
 2:run in simulator
 3:run in iphone"
 
+
+derivedDataPath=$project_path/ZLMediaKit/build/derivedData
+desLibPath=$derivedDataPath/ArchiveIntermediates/ALL_BUILD/IntermediateBuildFilesPath/UninstalledProducts
 cd ZLMediaKit
 
 read number
@@ -16,9 +19,17 @@ mkdir build
 cd build
 cmake .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake  -DPLATFORM=OS
 project_name=$(ls ./ | grep '.xcodeproj')
-xcodebuild archive -project ${project_name} -scheme ALL_BUILD -configuration Debug
+xcodebuild archive -project ${project_name} -scheme ALL_BUILD -configuration Debug -destination 'platform=iOS Simulator,name=iPhone 11' -derivedDataPath $derivedDataPath
 cmake .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake  -DPLATFORM=SIMULATOR
-xcodebuild archive -project ${project_name} -scheme ALL_BUILD -configuration Debug
+xcodebuild archive -project ${project_name} -scheme ALL_BUILD -configuration Debug -destination 'platform=iOS Simulator,name=iPhone 11' -derivedDataPath $derivedDataPath
+
+# sdks_path=${project_path}/IOSPlayer/SDKs
+# rm -rf $sdks_path
+# mkdir $sdks_path
+
+
+# lipo -create file.a file.b -output file.c
+
 else
 
 echo "end"
