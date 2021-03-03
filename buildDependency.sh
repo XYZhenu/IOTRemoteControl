dab2e41b60ab9435cf43ecae15522ea15d0dd584
XIEY071 <yan.x.xie.-nd@disney.com>
2021-03-03 18:15:26 +0800

    update script

--- buildDependency.sh
+++ buildDependency.sh
@@ -2,7 +2,8 @@ project_path=$(cd `dirname $0`; pwd)
 echo "Place enter the number you want do. 
 1:build iphone
 2:build simulator
-3:merge to fat"
+3:merge to fat
+4:build mac"
 
 
 derivedDataPath=$project_path/ZLMediaKit/build/derivedData
@@ -18,7 +19,7 @@ cd $project_path/ZLMediaKit
 rm -rf build
 mkdir build
 cd build
-cmake .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake  -DPLATFORM=OS
+cmake .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -DOPENSSL_ROOT_DIR=/usr/local/Cellar/openssl/1.0.2s/ -DPLATFORM=OS
 project_name=$(ls ./ | grep '.xcodeproj')
 xcodebuild archive -project ${project_name} -scheme ALL_BUILD -configuration Debug -destination 'platform=iOS Simulator,name=iPhone 11' -derivedDataPath $derivedDataPath
 rm -rf $productPath/iphoneos
@@ -30,7 +31,7 @@ cd $project_path/ZLMediaKit
 rm -rf build
 mkdir build
 cd build
-cmake .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake  -DPLATFORM=SIMULATOR64
+cmake .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -DOPENSSL_ROOT_DIR=/usr/local/Cellar/openssl/1.0.2s/  -DPLATFORM=SIMULATOR64
 project_name=$(ls ./ | grep '.xcodeproj')
 xcodebuild archive -project ${project_name} -scheme ALL_BUILD -configuration Debug -destination 'platform=iOS Simulator,name=iPhone 11' -derivedDataPath $derivedDataPath
 rm -rf $productPath/iphonesimulator
@@ -48,6 +49,16 @@ do
 done
 rm -rf $productPath/iphone
 ln -s $productPath/iphoneuniversal $productPath/iphone
+
+elif [ $number == 4 ]; then
+
+cd $project_path/ZLMediaKit
+rm -rf build
+mkdir build
+cd build
+cmake .. -DOPENSSL_ROOT_DIR=/usr/local/Cellar/openssl/1.0.2s/
+make -j4
+
 else
 
 echo "end"
