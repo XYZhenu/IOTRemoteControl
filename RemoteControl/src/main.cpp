9487fd1e1ba9fbedb7917bb016926bbc492dca98
xie xie <1515489649@qq.com>
2021-03-15 08:21:57 +0800

    update control

--- RemoteControl/src/main.cpp
+++ RemoteControl/src/main.cpp
@@ -5,6 +5,7 @@
 char ssid[] = "CMCC-kP6g";
 char pass[] = "p7bbkh3c";
 char ipadd[] = "192.168.1.13";
+char NODEMCU[] = "NODEMCU";
 WiFiClient net;
 MQTTClient client;
 
@@ -27,32 +28,82 @@ void connect() {
   client.subscribe("direction");
 }
 
+int nodemcuA1 = 5;
+int nodemcuA2 = 0;
+int nodemcuB1 = 4;
+int nodemcuB2 = 2;
+
+void mcuSetup()
+{
+
+
+  if (BOARD == NODEMCU)
+  {
+    pinMode(nodemcuA1, OUTPUT);
+    pinMode(nodemcuA2, OUTPUT);
+    pinMode(nodemcuB1, OUTPUT);
+    pinMode(nodemcuB2, OUTPUT);
+  }
+}
+
+void processYpwm(int pwm) {
+  if (BOARD == NODEMCU)
+  {
+    if (pwm > 0)
+    {
+      digitalWrite(nodemcuA1, LOW);
+      digitalWrite(nodemcuA2, HIGH);
+    } else {
+      digitalWrite(nodemcuA1, HIGH);
+      digitalWrite(nodemcuA2, LOW);
+    }
+  }
+  
+}
+
+void processXpwm(int pwm) {
+  if (BOARD == NODEMCU)
+  {
+    if (pwm > 0)
+    {
+      digitalWrite(nodemcuB1, LOW);
+      digitalWrite(nodemcuB2, HIGH);
+    } else {
+      digitalWrite(nodemcuB1, HIGH);
+      digitalWrite(nodemcuB2, LOW);
+    }
+  }
+  
+}
+
+void reset() {
+  if (BOARD == NODEMCU)
+  {
+    digitalWrite(nodemcuA1, LOW);
+    digitalWrite(nodemcuA2, LOW);
+    digitalWrite(nodemcuB1, LOW);
+    digitalWrite(nodemcuB2, LOW);
+  }
+}
+
 void messageReceived(String &topic, String &payload) {
   Serial.println("incoming: " + topic + " - " + payload);
   if (topic == "reset" && payload == "direction")
   {
-    
-  } else if (topic == "direction")
-  {
+    reset();
+  } else if (topic == "direction") {
     int index = payload.indexOf(",");
     String xstring = payload.substring(0,index);
     String ystring = payload.substring(index+1,(int)payload.length());
     int x = xstring.toInt();
     int y = ystring.toInt();
-
+    processXpwm(x);
+    processYpwm(y);
   }
 }
 
 
-void mcuSetup()
-{
-  //初始化电机驱动IO为输出方式
-  // pinMode(Left_motor_front, OUTPUT);
-  // pinMode(Left_motor_back, OUTPUT);
-  // digitalWrite(Right_motor_en, LOW);
-  // digitalWrite(Left_motor_en, LOW);
 
-}
 
 
 
